f388c821d2107da52d5b033d5c298d6b4ebdd9c6
Gerard Bruwn <siribby@outlook.com>
2015-01-09 16:23:19 -0800

    Add alternate flint and marcasite recipe
    Add French localization

--- src/main/java/genesis/common/GenesisRecipes.java
+++ src/main/java/genesis/common/GenesisRecipes.java
@@ -1,11 +1,13 @@
 package genesis.common;
 
 import genesis.item.EnumNodule;
+import genesis.item.EnumPebble;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public final class GenesisRecipes {
     protected static void addRecipes() {
+        GameRegistry.addShapelessRecipe(new ItemStack(GenesisItems.flint_and_marcasite), new ItemStack(GenesisItems.nodule, 1, EnumNodule.MARCASITE.getMetadata()), new ItemStack(GenesisItems.pebble, 1, EnumPebble.BROWN_FLINT.getMetadata()));
         GameRegistry.addShapelessRecipe(new ItemStack(GenesisItems.flint_and_marcasite), new ItemStack(GenesisItems.nodule, 1, EnumNodule.MARCASITE.getMetadata()), new ItemStack(GenesisItems.nodule, 1, EnumNodule.BROWN_FLINT.getMetadata()));
     }
 }
