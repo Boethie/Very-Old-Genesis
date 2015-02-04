42b162543cfe0ebae0dbfd094ef3b72b2055c506
Gerard Bruwn <siribby@outlook.com>
2015-02-03 22:38:56 -0800

    Add access transformer

---
+++ src/main/java/genesis/core/GenesisCore.java
@@ -0,0 +1,46 @@
+package genesis.core;
+
+import genesis.util.Constants;
+
+import java.io.IOException;
+import java.util.Map;
+
+import net.minecraftforge.fml.common.asm.transformers.AccessTransformer;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+public class GenesisCore extends AccessTransformer implements IFMLLoadingPlugin
+{
+	public GenesisCore() throws IOException
+	{
+		super(Constants.MOD_ID + "_at.cfg");
+	}
+	
+	@Override
+	public String[] getASMTransformerClass()
+	{
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass()
+	{
+		return null;
+	}
+
+	@Override
+	public String getSetupClass()
+	{
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data)
+	{
+	}
+
+	@Override
+	public String getAccessTransformerClass()
+	{
+		return getClass().getName();
+	}
+}
