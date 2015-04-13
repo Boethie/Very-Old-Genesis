9c318e7a774938ede73aa07960892d03cf1bd3c3
Zaggy1024 <Zaggy1024@gmail.com>
2015-04-13 16:26:28 -0500

    Made buckets use the recipe substitution method rather than only registering a cake recipe. Hopefully this will make any future recipes in the game, as well as mod recipes, work immediately.

--- src/main/java/genesis/common/GenesisRecipes.java
+++ src/main/java/genesis/common/GenesisRecipes.java
@@ -172,7 +172,6 @@ public final class GenesisRecipes
 		GameRegistry.addShapedRecipe(new ItemStack(GenesisBlocks.calamites_torch, 4), "x", "y", 'x', Items.coal, 'y', GenesisItems.calamites);
 		GameRegistry.addShapedRecipe(new ItemStack(GenesisBlocks.calamites_torch, 4), "x", "y", 'x', new ItemStack(Items.coal, 1, 1), 'y', GenesisItems.calamites);
 		GameRegistry.addShapedRecipe(new ItemStack(GenesisBlocks.calamites_torch, 4), "x", "y", 'x', GenesisItems.resin, 'y', GenesisItems.calamites);
-		GameRegistry.addShapedRecipe(new ItemStack(Items.cake), "xxx", "yzy", "ccc", 'x', GenesisItems.ceramic_bucket_milk, 'y', Items.sugar, 'z', Items.egg, 'c', Items.wheat);
 		GameRegistry.addShapelessRecipe(new ItemStack(GenesisItems.flint_and_marcasite), Metadata.newStack(EnumNodule.MARCASITE), Metadata.newStack(EnumPebble.BROWN_FLINT));
 		GameRegistry.addShapelessRecipe(new ItemStack(GenesisItems.flint_and_marcasite), Metadata.newStack(EnumNodule.MARCASITE), Metadata.newStack(EnumNodule.BROWN_FLINT));
 		GameRegistry.addShapelessRecipe(new ItemStack(GenesisItems.calamites, 9), GenesisBlocks.calamites_bundle);
@@ -201,5 +200,10 @@ public final class GenesisRecipes
 	public static void doSubstitutes()
 	{
 		makeSubstituteCraftingItem(new ItemStack(Items.string), new ItemStack(GenesisItems.sphenophyllum_fiber), Blocks.wool);
+		
+		// Buckets
+		makeSubstituteCraftingItem(new ItemStack(Items.bucket), new ItemStack(GenesisItems.ceramic_bucket));
+		makeSubstituteCraftingItem(new ItemStack(Items.water_bucket), new ItemStack(GenesisItems.ceramic_bucket_water));
+		makeSubstituteCraftingItem(new ItemStack(Items.milk_bucket), new ItemStack(GenesisItems.ceramic_bucket_milk));
 	}
 }
