e4f0da499b4fb2d8de793b96e06fc1e17f86f2b4
Zaggy1024 <Zaggy1024@gmail.com>
2015-05-10 19:54:44 -0500

    Fixed the ceramic buckets so that they give the right kind of buckets. Added dye textures. Updated ceramics texture.

--- src/main/java/genesis/item/ItemGenesisBucket.java
+++ src/main/java/genesis/item/ItemGenesisBucket.java
@@ -3,11 +3,18 @@ package genesis.item;
 import genesis.common.*;
 import genesis.util.*;
 import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.*;
+import net.minecraft.stats.StatList;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
 
 public class ItemGenesisBucket extends ItemBucket
 {
@@ -31,6 +38,96 @@ public class ItemGenesisBucket extends ItemBucket
 	{
 		return containedBlock == Blocks.air;
 	}
+
+    protected ItemStack fillBucket(ItemStack emptyBucket, EntityPlayer player, Item fullItem)
+    {
+        if (player.capabilities.isCreativeMode)
+        {
+            return emptyBucket;
+        }
+        else
+        {
+        	ItemStack full = new ItemStack(fullItem);
+        	
+	        if (--emptyBucket.stackSize <= 0)
+	        {
+	            return full;
+	        }
+	        else
+	        {
+	            if (!player.inventory.addItemStackToInventory(full))
+	            {
+	                player.dropPlayerItemWithRandomChoice(full, false);
+	            }
+	
+	            return emptyBucket;
+	        }
+        }
+    }
+	
+	@Override
+	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
+	{
+		boolean empty = isEmpty();
+		MovingObjectPosition hit = getMovingObjectPositionFromPlayer(world, player, empty);
+		
+		if (hit == null)
+		{
+			return stack;
+		}
+		
+		ItemStack eventOutput = net.minecraftforge.event.ForgeEventFactory.onBucketUse(player, world, stack, hit);
+		
+		if (eventOutput != null)
+		{
+			return eventOutput;
+		}
+		
+		if (hit.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+		{
+			BlockPos hitPos = hit.getBlockPos();
+			
+			if (!world.isBlockModifiable(player, hitPos))
+			{
+				return stack;
+			}
+			
+			if (empty)
+			{
+				if (!player.canPlayerEdit(hitPos.offset(hit.sideHit), hit.sideHit, stack))
+				{
+					return stack;
+				}
+				
+				IBlockState hitState = world.getBlockState(hitPos);
+				Material hitMaterial = hitState.getBlock().getMaterial();
+				
+				if (hitMaterial == Material.water && ((Integer) hitState.getValue(BlockLiquid.LEVEL)).intValue() == 0)
+				{
+					world.setBlockToAir(hitPos);
+					player.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
+					return fillBucket(stack, player, GenesisItems.ceramic_bucket_water);
+				}
+			}
+			else
+			{
+				BlockPos placePos = hitPos.offset(hit.sideHit);
+				
+				if (!player.canPlayerEdit(placePos, hit.sideHit, stack))
+				{
+					return stack;
+				}
+				
+				if (tryPlaceContainedLiquid(world, placePos) && !player.capabilities.isCreativeMode)
+				{
+					player.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
+					return new ItemStack(GenesisItems.ceramic_bucket);
+				}
+			}
+		}
+		
+		return stack;
+	}
 	
 	@Override
 	public boolean itemInteractionForEntity(ItemStack stack, EntityPlayer playerIn, EntityLivingBase target)
@@ -43,7 +140,7 @@ public class ItemGenesisBucket extends ItemBucket
 			}
 			else
 			{
-				--stack.stackSize;
+				stack.stackSize--;
 				playerIn.inventory.addItemStackToInventory(new ItemStack(GenesisItems.ceramic_bucket_milk));
 			}
 		}
