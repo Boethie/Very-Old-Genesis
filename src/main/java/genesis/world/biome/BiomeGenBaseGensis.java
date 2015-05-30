2e016f15dc3557a24d4c4d33f68ea903607b2eca
superckl <clarsson@wisc.edu>
2015-05-30 00:58:52 -0700

    Adding biome color method

--- src/main/java/genesis/world/biome/BiomeGenBaseGensis.java
+++ src/main/java/genesis/world/biome/BiomeGenBaseGensis.java
@@ -4,6 +4,7 @@ import genesis.block.BlockMoss;
 import genesis.common.GenesisBlocks;
 import genesis.world.biome.decorate.BiomeDecoratorGenesis;
 import net.minecraft.block.BlockGrass;
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public abstract class BiomeGenBaseGensis extends BiomeGenBase
@@ -22,4 +23,12 @@ public abstract class BiomeGenBaseGensis extends BiomeGenBase
 		this.waterColorMultiplier = 0xaa791e;
 	}
 
+	@Override
+	public int getFoliageColorAtPos(BlockPos p_180625_1_)
+	{
+		return 0x015a03;
+	}
+
+	
+	
 }
