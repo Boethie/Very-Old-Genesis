128ca5bfd638712d1080232ed2d8455701db59b1
Winter Grave <paulmongeurquijo@gmail.com>
2015-07-20 07:52:01 -0700

    Commit local to pull

---
+++ src/main/java/genesis/world/biome/decorate/WorldGenArchaeomarasmius.java
@@ -0,0 +1,40 @@
+package genesis.world.biome.decorate;
+
+import genesis.common.GenesisBlocks;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
+public class WorldGenArchaeomarasmius extends WorldGenDecorationBase
+{
+	@Override
+	public boolean generate(World world, Random random, BlockPos pos)
+	{
+		Block block;
+		
+		do
+		{
+			block = world.getBlockState(pos).getBlock();
+			if (!block.isLeaves(world, pos) && !block.isLeaves(world, pos))
+			{
+				break;
+			}
+			pos = pos.down();
+		}
+		while (pos.getY() > 0);
+		
+		if (!(world.getBlockState(pos).getBlock() == GenesisBlocks.moss || world.getBlockState(pos).getBlock() == Blocks.dirt))
+			return false;
+		
+		if (world.getLight(pos.up()) > 14)
+			return false;
+		
+		setBlockInWorld(world, pos.up(), GenesisBlocks.archaeomarasmius.getDefaultState());
+		
+		return true;
+	}
+}
