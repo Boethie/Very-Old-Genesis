22bd10be841b86e5154191962817763ef2f5cfc5
Zaggy1024 <Zaggy1024@gmail.com>
2015-08-06 16:07:00 -0500

    Made the ceramic water bucket fill cauldrons.

--- src/main/java/genesis/item/ItemGenesisBucket.java
+++ src/main/java/genesis/item/ItemGenesisBucket.java
@@ -1,21 +1,31 @@
 package genesis.item;
 
+import genesis.client.GenesisClient;
 import genesis.common.*;
 import genesis.util.*;
 import genesis.util.Constants.Unlocalized;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityCow;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.*;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
 import net.minecraft.stats.StatList;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemGenesisBucket extends ItemBucket
 {
@@ -33,6 +43,11 @@ public class ItemGenesisBucket extends ItemBucket
 		}
 		
 		setCreativeTab(GenesisCreativeTabs.MISC);
+		
+		if (!isEmpty() && containedBlock.getMaterial() == Material.water)
+		{
+			MinecraftForge.EVENT_BUS.register(this);
+		}
 	}
 	
 	public boolean isEmpty()
@@ -93,6 +108,9 @@ public class ItemGenesisBucket extends ItemBucket
 				return stack;
 			}
 			
+			IBlockState hitState = world.getBlockState(hitPos);
+			Block hitBlock = hitState.getBlock();
+			
 			if (empty)
 			{
 				if (!player.canPlayerEdit(hitPos.offset(hit.sideHit), hit.sideHit, stack))
@@ -100,10 +118,7 @@ public class ItemGenesisBucket extends ItemBucket
 					return stack;
 				}
 				
-				IBlockState hitState = world.getBlockState(hitPos);
-				Material hitMaterial = hitState.getBlock().getMaterial();
-				
-				if (hitMaterial == Material.water && ((Integer) hitState.getValue(BlockLiquid.LEVEL)).intValue() == 0)
+				if (hitBlock.getMaterial() == Material.water && ((Integer) hitState.getValue(BlockLiquid.LEVEL)).intValue() == 0)
 				{
 					world.setBlockToAir(hitPos);
 					player.triggerAchievement(StatList.objectUseStats[Item.getIdFromItem(this)]);
@@ -147,5 +162,88 @@ public class ItemGenesisBucket extends ItemBucket
 		}
 		
 		return true;
+	}
+	
+	protected boolean filledWater = false;
+	
+	@SubscribeEvent
+	public void onBlockInteracted(PlayerInteractEvent event)
+	{
+		EntityPlayer player = event.entityPlayer;
+		
+		World world = event.world;
+		BlockPos pos = event.pos;
+		
+		IBlockState state = Blocks.air.getDefaultState();
+		if (pos != null)
+			state = world.getBlockState(pos);
+		Block block = state.getBlock();
+		
+		ItemStack stack = player.getHeldItem();
+		
+		switch (event.action)
+		{
+		case RIGHT_CLICK_BLOCK:
+			if (stack != null && stack.getItem() == this && block == Blocks.cauldron)
+			{
+				event.useBlock = Result.DENY;
+				
+				int cauldronLevel = (Integer) state.getValue(BlockCauldron.LEVEL);
+				int max = 0;
+				
+				for (Object val : BlockCauldron.LEVEL.getAllowedValues())
+				{
+					int intVal = (Integer) val;
+					
+					if (intVal > max)
+					{
+						max = intVal;
+					}
+				}
+				
+				if (cauldronLevel < max)
+				{
+					world.setBlockState(pos, state.withProperty(BlockCauldron.LEVEL, Math.min(cauldronLevel + 3, max)));
+					
+					if (!player.capabilities.isCreativeMode)
+					{
+						player.inventory.setInventorySlotContents(player.inventory.currentItem, new ItemStack(GenesisItems.ceramic_bucket));
+					}
+					
+					event.useItem = Result.DENY;
+					
+					if (world.isRemote)	// We must send a packet to the server telling it that the player right clicked or else it won't fill the cauldron server side.
+					{
+						filledWater = true;	// Used to prevent RIGHT_CLICK_AIR going through and placing water in onItemRightClick
+						
+						Minecraft mc = GenesisClient.getMC();
+						EntityPlayerSP spPlayer = mc.thePlayer;
+						
+						if (spPlayer == player)
+						{
+							Vec3 hitVec = mc.objectMouseOver.hitVec;
+							hitVec = hitVec.subtract(pos.getX(), pos.getY(), pos.getZ());
+							Packet packet = new C08PacketPlayerBlockPlacement(pos, event.face.getIndex(), stack, (float) hitVec.xCoord, (float) hitVec.yCoord, (float) hitVec.zCoord);
+							spPlayer.sendQueue.addToSendQueue(packet);
+							
+							spPlayer.swingItem();
+							event.setCanceled(true);
+						}
+					}
+				}
+			}
+			
+			break;
+		case RIGHT_CLICK_AIR:
+			if (stack != null && stack.getItem() == this && filledWater)
+			{
+				event.setCanceled(true);
+				filledWater = false;
+			}
+			
+			break;
+		case LEFT_CLICK_BLOCK:
+			break;
+		}
 	}
 }
