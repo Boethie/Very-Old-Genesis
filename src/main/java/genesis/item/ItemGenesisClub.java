e1d2039d18f2b62ea18bebcc2428e508bf50604e
Zaggy1024 <Zaggy1024@gmail.com>
2015-09-27 21:37:33 -0500

    Added an item class for the club.

---
+++ src/main/java/genesis/item/ItemGenesisClub.java
@@ -0,0 +1,56 @@
+package genesis.item;
+
+import genesis.metadata.ToolItems;
+import genesis.metadata.ToolItems.ToolObjectType;
+import genesis.metadata.ToolTypes.ToolType;
+import genesis.metadata.VariantsOfTypesCombo.ItemVariantCount;
+
+import java.util.List;
+
+import com.google.common.collect.Multimap;
+
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.*;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+@ItemVariantCount(1)
+public class ItemGenesisClub extends ItemSword
+{
+	public final ToolItems owner;
+	
+	protected final ToolType type;
+	protected final ToolObjectType objType;
+	
+	public ItemGenesisClub(ToolType type, ToolItems owner, ToolObjectType objType)
+	{
+		super(type.toolMaterial);
+		
+		this.owner = owner;
+		this.type = type;
+		this.objType = objType;
+	}
+	
+	@Override
+	public String getUnlocalizedName(ItemStack stack)
+	{
+		return owner.getUnlocalizedName(stack, super.getUnlocalizedName(stack));
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack, EntityPlayer playerIn, List tooltip, boolean advanced)
+	{
+		super.addInformation(stack, playerIn, tooltip, advanced);
+		owner.addToolInformation(stack, playerIn, tooltip, advanced);
+	}
+	
+	@Override
+	public Multimap getItemAttributeModifiers()
+	{
+		Multimap modifiers = super.getItemAttributeModifiers();
+		modifiers.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(),
+				new AttributeModifier(itemModifierUUID, "Weapon modifier", 3.5F + type.toolMaterial.getDamageVsEntity(), 0));
+		return modifiers;
+	}
+}
