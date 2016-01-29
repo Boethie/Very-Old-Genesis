7bb626b46454418b64a55ea74de739c899c82093
Zaggy1024 <Zaggy1024@gmail.com>
2016-01-29 13:18:28 -0600

    Fixed unused import.

--- src/main/java/genesis/block/BlockHangingFruit.java
+++ src/main/java/genesis/block/BlockHangingFruit.java
@@ -17,7 +17,6 @@ import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
