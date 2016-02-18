1d2c50aa81e420da4fb3839e49fc9349597e830f
Zaggy1024 <Zaggy1024@gmail.com>
2016-02-18 17:58:06 -0600

    Added cordaites smelting recipe. Renamed pile textures to pile_[variant].

--- src/main/java/genesis/common/GenesisRecipes.java
+++ src/main/java/genesis/common/GenesisRecipes.java
@@ -663,6 +663,8 @@ public final class GenesisRecipes
 		ItemStack ash = GenesisItems.materials.getStack(EnumMaterial.VEGETAL_ASH);
 		TileEntityCampfire.registerAllowedOutput(ash);
 		GameRegistry.addSmelting(GenesisBlocks.plants.getPlantStack(EnumPlant.LEPACYCLOTES), ash, 0.15F);
+		GameRegistry.addSmelting(GenesisBlocks.trees.getStack(TreeBlocksAndItems.SAPLING, EnumTree.CORDAITES), ash, 0.15F);
+		CookingPotRecipeRegistry.registerShapeless(GenesisItems.materials.getStack(EnumMaterial.SALT), ash);
 		
 		// Pottery
 		GameRegistry.addRecipe(new ItemStack(GenesisBlocks.red_clay), "CC", "CC", 'C', GenesisItems.red_clay_ball);
