200ffd56a810b73ac79658dad123452d7839a55d
Vorquel <vorquel@gmail.com>
2016-03-10 13:31:21 -0700

    Make trap floors flammable

--- src/main/java/genesis/block/BlockTrapFloor.java
+++ src/main/java/genesis/block/BlockTrapFloor.java
@@ -3,6 +3,7 @@ package genesis.block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.ColorizerGrass;
 import net.minecraft.world.IBlockAccess;
@@ -32,24 +33,34 @@ public class BlockTrapFloor extends BlockGenesis
 		return false;
 	}
 	
+	@Override
+	public int getFlammability(IBlockAccess world, BlockPos pos, EnumFacing face)
+	{
+		return 30;
+	}
+	
+	@Override
 	@SideOnly(Side.CLIENT)
 	public int getBlockColor()
 	{
 		return ColorizerGrass.getGrassColor(0.5D, 1.0D);
 	}
 	
+	@Override
 	@SideOnly(Side.CLIENT)
 	public int getRenderColor(IBlockState state)
 	{
 		return this.getBlockColor();
 	}
 	
+	@Override
 	@SideOnly(Side.CLIENT)
 	public int colorMultiplier(IBlockAccess worldIn, BlockPos pos, int renderPass)
 	{
 		return BiomeColorHelper.getGrassColorAtPos(worldIn, pos);
 	}
 	
+	@Override
 	@SideOnly(Side.CLIENT)
 	public EnumWorldBlockLayer getBlockLayer()
 	{
