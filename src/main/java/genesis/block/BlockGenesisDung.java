466ac4fd06a13fb6b38fe8f25b31303a05ea0928
Vorquel <vorquel@gmail.com>
2016-03-17 14:04:43 -0700

    Dung now has a height property
    Height goes from 1 to 8, defaults to 8.

--- src/main/java/genesis/block/BlockGenesisDung.java
+++ src/main/java/genesis/block/BlockGenesisDung.java
@@ -5,6 +5,8 @@ import genesis.combo.VariantsOfTypesCombo.BlockProperties;
 import genesis.combo.variant.EnumDung;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.util.AxisAlignedBB;
@@ -15,20 +17,34 @@ import java.util.List;
 
 public class BlockGenesisDung extends BlockGenesisVariants<EnumDung>
 {
+	public static final PropertyInteger HEIGHT = PropertyInteger.create("height", 1, 8);
 	@BlockProperties
 	public static IProperty<?>[] getProperties()
 	{
-		return new IProperty[]{};
+		return new IProperty[]{ HEIGHT };
 	}
 	
 	public BlockGenesisDung(VariantsOfTypesCombo<EnumDung> owner, VariantsOfTypesCombo.ObjectType<? extends BlockGenesisVariants<EnumDung>, ? extends Item> type, List<EnumDung> variants, Class<EnumDung> variantClass, Material material)
 	{
 		super(owner, type, variants, variantClass, material);
+		blockState = new BlockState(this, variantProp, HEIGHT);
+		setDefaultState(blockState.getBaseState().withProperty(HEIGHT, 8));
 	}
 	
 	@Override
 	public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state)
 	{
-		return new AxisAlignedBB((double)pos.getX() + this.minX, (double)pos.getY() + this.minY, (double)pos.getZ() + this.minZ, (double)pos.getX() + this.maxX, (double)pos.getY() + this.maxY - 1/8D, (double)pos.getZ() + this.maxZ);
+		return new AxisAlignedBB(
+				pos.getX() + this.minX, pos.getY() + this.minY, pos.getZ() + this.minZ,
+				pos.getX() + this.maxX, pos.getY() + (state.getValue(HEIGHT) - 1)/8D, pos.getZ() + this.maxZ);
+	}
+	
+	@Override
+	public AxisAlignedBB getSelectedBoundingBox(World worldIn, BlockPos pos)
+	{
+		IBlockState state = worldIn.getBlockState(pos);
+		return new AxisAlignedBB(
+				pos.getX() + this.minX, pos.getY() + this.minY, pos.getZ() + this.minZ,
+				pos.getX() + this.maxX, pos.getY() + state.getValue(HEIGHT)/8D, pos.getZ() + this.maxZ);
 	}
 }
