949426037a578aec96f4c4111da8a40c79367d42
Anaghold <benjaminbeaulieu27@gmail.com>
2016-03-20 01:45:56 +0100

    Reworked Meta Forest biome.

--- src/main/java/genesis/world/biome/BiomeGenMetaForestM.java
+++ src/main/java/genesis/world/biome/BiomeGenMetaForestM.java
@@ -15,7 +15,7 @@ public class BiomeGenMetaForestM extends BiomeGenMetaForest
 	
 	protected void addDecorations()
 	{
-		addDecoration(new WorldGenRockBoulders().setWaterRequired(false).setMaxHeight(3).setRarity(4).setCountPerChunk(1));
+		addDecoration(new WorldGenRockBoulders().setWaterRequired(false).setMaxHeight(3).setRarity(3).setCountPerChunk(1));
 		addDecoration(new WorldGenRockBoulders().setWaterRequired(false).setMaxHeight(3).addBlocks(GenesisBlocks.rhyolite.getDefaultState()).setRarity(9).setCountPerChunk(1));
 		addDecoration(new WorldGenRockBoulders().setWaterRequired(false).setMaxHeight(3).addBlocks(GenesisBlocks.dolerite.getDefaultState()).setRarity(9).setCountPerChunk(1));
 		addDecoration(new WorldGenDebris().setCountPerChunk(7));
