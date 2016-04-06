7416453f4382d4adc817a4aae5858b57a50702ac
Zaggy1024 <Zaggy1024@gmail.com>
2016-04-06 16:18:05 -0500

    Cleaned up BlockRack, TileEntityRack and TileEntityRackRenderer.

--- src/main/java/genesis/block/tileentity/TileEntityRack.java
+++ src/main/java/genesis/block/tileentity/TileEntityRack.java
@@ -19,208 +19,209 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class TileEntityRack extends TileEntity implements IInventory
 {
+	private ItemStack[] inventory = new ItemStack[1];
+	
+	public static boolean isItemValid(ItemStack stack)
+	{
+		if (stack != null)
+		{
+			Item item = stack.getItem();
+			
+			return (item instanceof ItemBlock)
+					|| (item instanceof ItemPotion)
+					|| (item instanceof ItemBucketMilk)
+					|| (item instanceof ItemSoup)
+					|| item == Items.lava_bucket
+					|| item == Items.water_bucket
+					|| item == Items.cake;
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public Packet<?> getDescriptionPacket()
+	{
+		NBTTagCompound data = new NBTTagCompound();
+		writeToNBT(data);
+		return new SPacketUpdateTileEntity(pos, 1, data);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet)
+	{
+		readFromNBT(packet.getNbtCompound());
+		this.markDirty();
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound)
+	{
+		super.readFromNBT(compound);
+		
+		NBTTagList list = compound.getTagList("items", 10);
+		inventory = new ItemStack[getSizeInventory()];
+		
+		for (int i = 0; i < list.tagCount(); ++i)
+		{
+			NBTTagCompound itemCompound = list.getCompoundTagAt(i);
+			int slot = itemCompound.getByte("slot");
+			
+			if (slot >= 0 && slot < inventory.length)
+			{
+				inventory[slot] = ItemStack.loadItemStackFromNBT(itemCompound);
+			}
+		}
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound compound)
+	{
+		super.writeToNBT(compound);
+		
+		NBTTagList list = new NBTTagList();
+		
+		for (int i = 0; i < inventory.length; ++i)
+		{
+			if (inventory[i] != null)
+			{
+				NBTTagCompound itemCompound = new NBTTagCompound();
+				itemCompound.setByte("slot", (byte) i);
+				inventory[i].writeToNBT(itemCompound);
+				list.appendTag(itemCompound);
+			}
+		}
+		
+		compound.setTag("items", list);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public double getMaxRenderDistanceSquared()
+	{
+		return 65536;
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public AxisAlignedBB getRenderBoundingBox()
+	{
+		return new AxisAlignedBB(pos.getX() - 0.5, pos.getY(), pos.getZ() - 0.5,
+				pos.getX() + 1.5, pos.getY() + 1.5, pos.getZ() + 1.5);
+	}
+	
+	@Override
+	public String getName()
+	{
+		return null;
+	}
+	
+	@Override
+	public boolean hasCustomName()
+	{
+		return false;
+	}
+	
+	@Override
+	public ITextComponent getDisplayName()
+	{
+		return null;
+	}
+	
+	@Override
+	public int getSizeInventory()
+	{
+		return inventory.length;
+	}
+	
+	@Override
+	public int getInventoryStackLimit()
+	{
+		return 1;
+	}
+	
+	@Override
+	public ItemStack getStackInSlot(int index)
+	{
+		return inventory[index];
+	}
+	
+	public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction)
+	{
+		return isItemValidForSlot(index, itemStackIn);
+	}
+	
+	@Override
+	public ItemStack decrStackSize(int index, int count)
+	{
+		return ItemStackHelper.getAndSplit(inventory, index, count);
+	}
+	
+	@Override
+	public ItemStack removeStackFromSlot(int index)
+	{
+		return ItemStackHelper.getAndRemove(inventory, index);
+	}
+	
+	@Override
+	public void setInventorySlotContents(int index, ItemStack stack)
+	{
+		inventory[index] = stack;
+		
+		if (stack != null && stack.stackSize > getInventoryStackLimit())
+		{
+			stack.stackSize = getInventoryStackLimit();
+		}
+	}
+	
+	@Override
+	public boolean isUseableByPlayer(EntityPlayer player)
+	{
+		return false;
+	}
+	
+	@Override
+	public void openInventory(EntityPlayer player)
+	{
 
-    private ItemStack[] inventory = new ItemStack[1];
+	}
+	
+	@Override
+	public void closeInventory(EntityPlayer player)
+	{
 
-    public static boolean isItemValid(ItemStack stack)
-    {
-        if (stack != null)
-        {
-            boolean ret = (stack.getItem() instanceof ItemBlock) || (stack.getItem() instanceof ItemPotion) || (stack.getItem() instanceof ItemBucketMilk) || (stack.getItem() instanceof ItemRedstone) || (stack.getItem() instanceof ItemSoup);
-
-            //IF MASTER FAILS DO INDIVIDUAL ITEMS
-            if (!ret)
-            {
-                return (stack.getItem().equals(Items.lava_bucket)) || (stack.getItem().equals(Items.water_bucket)) || (stack.getItem().equals(Items.cake));
-            }
-            return ret;
-        }
-        return false;
-    }
-
-    @Override
-    public Packet<?> getDescriptionPacket()
-    {
-        NBTTagCompound data = new NBTTagCompound();
-        writeToNBT(data);
-        return new SPacketUpdateTileEntity(this.pos, 1, data);
-    }
-
-    @Override
-    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet)
-    {
-        readFromNBT(packet.getNbtCompound());
-        this.markDirty();
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound compound)
-    {
-        super.readFromNBT(compound);
-
-        NBTTagList nbttaglist = compound.getTagList("items", 10);
-        this.inventory = new ItemStack[this.getSizeInventory()];
-
-        for (int i = 0; i < nbttaglist.tagCount(); ++i)
-        {
-            NBTTagCompound nbttagcompound = nbttaglist.getCompoundTagAt(i);
-            int j = nbttagcompound.getByte("slot");
-
-            if (j >= 0 && j < this.inventory.length)
-            {
-                this.inventory[j] = ItemStack.loadItemStackFromNBT(nbttagcompound);
-            }
-        }
-    }
-
-    @Override
-    public void writeToNBT(NBTTagCompound compound)
-    {
-        super.writeToNBT(compound);
-
-        NBTTagList nbttaglist = new NBTTagList();
-
-        for (int i = 0; i < this.inventory.length; ++i)
-        {
-            if (this.inventory[i] != null)
-            {
-                NBTTagCompound nbttagcompound = new NBTTagCompound();
-                nbttagcompound.setByte("slot", (byte) i);
-                this.inventory[i].writeToNBT(nbttagcompound);
-                nbttaglist.appendTag(nbttagcompound);
-            }
-        }
-
-        compound.setTag("items", nbttaglist);
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public double getMaxRenderDistanceSquared()
-    {
-        return 65536.0D;
-    }
-
-    @Override
-    @SideOnly(Side.CLIENT)
-    public AxisAlignedBB getRenderBoundingBox()
-    {
-        return INFINITE_EXTENT_AABB;
-    }
-
-    @Override
-    public String getName()
-    {
-        return null;
-    }
-
-    @Override
-    public boolean hasCustomName()
-    {
-        return false;
-    }
-
-    @Override
-    public ITextComponent getDisplayName()
-    {
-        return null;
-    }
-
-    @Override
-    public int getSizeInventory()
-    {
-        return 1;
-    }
-
-    @Override
-    public ItemStack getStackInSlot(int index)
-    {
-        return this.inventory[index];
-    }
-
-    public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction)
-    {
-        return this.isItemValidForSlot(index, itemStackIn);
-    }
-
-    @Override
-    public ItemStack decrStackSize(int index, int count)
-    {
-        return ItemStackHelper.getAndSplit(this.inventory, index, count);
-    }
-
-    @Override
-    public ItemStack removeStackFromSlot(int index)
-    {
-        return ItemStackHelper.getAndRemove(this.inventory, index);
-    }
-
-    @Override
-    public void setInventorySlotContents(int index, ItemStack stack)
-    {
-        this.inventory[index] = stack;
-        if (stack != null && stack.stackSize > this.getInventoryStackLimit())
-        {
-            stack.stackSize = this.getInventoryStackLimit();
-        }
-    }
-
-    @Override
-    public int getInventoryStackLimit()
-    {
-        return 1;
-    }
-
-    @Override
-    public boolean isUseableByPlayer(EntityPlayer player)
-    {
-        return false;
-    }
-
-    @Override
-    public void openInventory(EntityPlayer player)
-    {
-
-    }
-
-    @Override
-    public void closeInventory(EntityPlayer player)
-    {
-
-    }
-
-    @Override
-    public boolean isItemValidForSlot(int index, ItemStack stack)
-    {
-        return stack != null && isItemValid(stack);
-    }
-
-    @Override
-    public int getField(int id)
-    {
-        return 0;
-    }
-
-    @Override
-    public void setField(int id, int value)
-    {
-    }
-
-    @Override
-    public int getFieldCount()
-    {
-        return 0;
-    }
-
-    @Override
-    public void clear()
-    {
-    }
-
-    public int getDirection()
-    {
-        return this.getBlockMetadata();
-    }
-
-
+	}
+	
+	@Override
+	public boolean isItemValidForSlot(int index, ItemStack stack)
+	{
+		return stack != null && isItemValid(stack);
+	}
+	
+	@Override
+	public int getField(int id)
+	{
+		return 0;
+	}
+	
+	@Override
+	public void setField(int id, int value)
+	{
+	}
+	
+	@Override
+	public int getFieldCount()
+	{
+		return 0;
+	}
+	
+	@Override
+	public void clear()
+	{
+	}
+	
+	public int getDirection()
+	{
+		return this.getBlockMetadata();
+	}
 }
