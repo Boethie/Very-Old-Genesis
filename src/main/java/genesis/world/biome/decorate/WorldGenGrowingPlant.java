adf2a91833500596a5aa66db7d3940415983fc20
Zaggy1024 <Zaggy1024@gmail.com>
2016-04-06 22:45:24 -0500

    Removed unused imports.

--- src/main/java/genesis/world/biome/decorate/WorldGenGrowingPlant.java
+++ src/main/java/genesis/world/biome/decorate/WorldGenGrowingPlant.java
@@ -3,12 +3,9 @@ package genesis.world.biome.decorate;
 import java.util.Random;
 
 import genesis.block.BlockGrowingPlant;
-import genesis.common.GenesisBlocks;
 import genesis.util.WorldBlockMatcher;
 import genesis.util.WorldUtils;
 
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
