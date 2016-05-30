5e54a37f07b7f8128c5340392a2fe3e2650ffe57
MrIbby <siribby@outlook.com>
2016-05-29 19:22:03 -0700

    Added cracked silt

--- src/main/java/genesis/combo/SiltBlocks.java
+++ src/main/java/genesis/combo/SiltBlocks.java
@@ -6,11 +6,16 @@ import genesis.block.*;
 import genesis.combo.variant.EnumSilt;
 import genesis.item.*;
 import genesis.util.Constants;
+import genesis.util.ReflectionUtils;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
 
 public class SiltBlocks extends VariantsOfTypesCombo<EnumSilt>
 {
 	public static final ObjectType<EnumSilt, BlockSilt, ItemBlockMulti<EnumSilt>> SILT =
 			ObjectType.createBlock(EnumSilt.class, "silt", BlockSilt.class);
+	public static final ObjectType<EnumSilt, BlockGenesisVariants<EnumSilt>, ItemBlockMulti<EnumSilt>> CRACKED_SILT =
+			new ObjectType<>(EnumSilt.class, "cracked_silt", ReflectionUtils.convertClass(BlockGenesisVariants.class), null);
 	public static final ObjectType<EnumSilt, BlockSiltstone, ItemBlockMulti<EnumSilt>> SILTSTONE =
 			ObjectType.createBlock(EnumSilt.class, "siltstone", "rock.siltstone", BlockSiltstone.class);
 	
@@ -18,13 +23,17 @@ public class SiltBlocks extends VariantsOfTypesCombo<EnumSilt>
 	{
 		SILT.setVariantNameFunction((v) -> v == EnumSilt.SILT ? SILT.getName() : SILT.getName() + "_" + v.getName())
 				.setUseSeparateVariantJsons(false);
+		CRACKED_SILT.setVariantNameFunction((v) -> v == EnumSilt.SILT ? CRACKED_SILT.getName() : CRACKED_SILT.getName() + "_" + v.getName())
+				.setConstructedFunction((b, i) -> b.setHardness(0.6F))
+				.setUseSeparateVariantJsons(false)
+				.setBlockArguments(Material.sand, SoundType.SAND);
 		SILTSTONE.setVariantNameFunction((v) -> v == EnumSilt.SILT ? SILTSTONE.getName() : SILTSTONE.getName() + "_" + v.getName())
 				.setUseSeparateVariantJsons(false);
 	}
 	
 	public SiltBlocks()
 	{
-		super("silt", ImmutableList.of(SILT, SILTSTONE),
+		super("silt", ImmutableList.of(SILT, CRACKED_SILT, SILTSTONE),
 				EnumSilt.class, ImmutableList.copyOf(EnumSilt.values()));
 		
 		setNames(Constants.MOD_ID, Constants.Unlocalized.PREFIX);
