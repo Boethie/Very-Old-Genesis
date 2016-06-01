765a949749bbbd6b86ce6e2a4f49919d2e31a02b
Winter Grave <paulmongeurquijo@gmail.com>
2016-05-31 17:07:09 -0700

    Fixed Stemonitis generation

--- src/main/java/genesis/world/biome/decorate/WorldGenStemonitis.java
+++ src/main/java/genesis/world/biome/decorate/WorldGenStemonitis.java
@@ -1,11 +1,10 @@
 package genesis.world.biome.decorate;
 
+import java.util.Random;
+
 import genesis.block.IMushroomBase;
 import genesis.common.GenesisBlocks;
 import genesis.util.functional.WorldBlockMatcher;
-
-import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
@@ -22,17 +21,17 @@ public class WorldGenStemonitis extends WorldGenDecorationBase
 	@Override
 	public boolean place(World world, Random random, BlockPos pos)
 	{
-		Block block = world.getBlockState(pos).getBlock();
-
+		Block block = world.getBlockState(pos.down()).getBlock();
+		
 		if (block instanceof IMushroomBase)
 		{
 			IMushroomBase base = (IMushroomBase) block;
 			IBlockState placedState = GenesisBlocks.stemonitis.getDefaultState();
-
+			
 			if (!base.canSustainMushroom(world, pos, EnumFacing.UP, placedState))
 				return false;
-
-			return setAirBlock(world, pos.up(), placedState);
+			
+			return setAirBlock(world, pos, placedState);
 		}
 		
 		return false;
