cb36e832b353e97c713926ac3c55d7a208258256
Artem226 <artemkhrapov2001@yandex.ru>
2016-06-03 17:55:27 +0300

    Added the effect for the radiaion
    Radiation reduces 5 hp from a player and destroys the player's luck(-5).
    
    Also it adds glowing effect and nausea.
    
    https://trello.com/c/QDU6G4k0/605-add-radioactive-granite

--- src/main/java/genesis/common/GenesisPotions.java
+++ src/main/java/genesis/common/GenesisPotions.java
@@ -3,6 +3,7 @@ package genesis.common;
 import genesis.potion.PotionGenesis;
 import genesis.util.Constants;
 import genesis.util.Constants.Unlocalized;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.GameRegistry;
@@ -12,7 +13,9 @@ public class GenesisPotions
 	public static final Potion radiation = new PotionGenesis(true, 0x00FF00)
 			.setIcon(new ResourceLocation(Constants.MOD_ID, "textures/gui/radiation.png"))
 			.setEffectiveness(0.25D)
-			.setPotionName(Unlocalized.EFFECT + "radiation");
+			.setPotionName(Unlocalized.EFFECT + "radiation")
+			.registerPotionAttributeModifier(SharedMonsterAttributes.MAX_HEALTH, "5D6F0BA2-1186-46AC-B896-C61C5CEE99CC", -5.0D, 0)
+			.registerPotionAttributeModifier(SharedMonsterAttributes.LUCK, "CC5AF142-2BD2-4215-B636-2605AED11727", -5.0D, 0);
 
 	private static ResourceLocation name(String path)
 	{
