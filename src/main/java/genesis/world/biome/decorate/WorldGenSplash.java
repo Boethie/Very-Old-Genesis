bff7cc4019d8c67c5191c2d3ddb5855e0cb53e08
Zaggy1024 <Zaggy1024@gmail.com>
2016-06-06 07:01:02 -0500

    Removed unused imports from WorldGenSplash.

--- src/main/java/genesis/world/biome/decorate/WorldGenSplash.java
+++ src/main/java/genesis/world/biome/decorate/WorldGenSplash.java
@@ -5,9 +5,7 @@
 package genesis.world.biome.decorate;
 
 import genesis.util.functional.WorldBlockMatcher;
-import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
