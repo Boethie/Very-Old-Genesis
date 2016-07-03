3782d4067fe9e9ac0d4a7502a684c9a5631ca92f
Anaghold <benjaminbeaulieu27@gmail.com>
2016-07-03 03:56:16 +0200

    Added addDecorations for M biomes.

--- src/main/java/genesis/world/biome/BiomeGenMetaForestM.java
+++ src/main/java/genesis/world/biome/BiomeGenMetaForestM.java
@@ -1,12 +1,8 @@
 package genesis.world.biome;
 
-import genesis.combo.variant.EnumDung;
 import genesis.common.GenesisBlocks;
 import genesis.util.random.f.FloatRange;
 import genesis.world.biome.decorate.WorldGenBoulders;
-import genesis.world.biome.decorate.WorldGenDebris;
-import genesis.world.biome.decorate.WorldGenDung;
-import genesis.world.biome.decorate.WorldGenSplash;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class BiomeGenMetaForestM extends BiomeGenMetaForest
@@ -14,18 +10,16 @@ public class BiomeGenMetaForestM extends BiomeGenMetaForest
 	public BiomeGenMetaForestM(BiomeGenBase.BiomeProperties properties)
 	{
 		super(properties);
+		
+		super.addDecorations();
 	}
 	
 	@Override
 	protected void addDecorations()
 	{
-		addDecoration(WorldGenSplash.createHumusSplash(), 2.1F);
 		addDecoration(new WorldGenBoulders(1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.153F);
 		addDecoration(new WorldGenBoulders(GenesisBlocks.rhyolite.getDefaultState(), 1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.075F);
 		addDecoration(new WorldGenBoulders(GenesisBlocks.dolerite.getDefaultState(), 1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.075F);
-		addDecoration(new WorldGenDung(EnumDung.HERBIVORE), 0.08F);
-		
-		addPostDecoration(new WorldGenDebris(), 7);
 	}
 	
 	@Override
--- src/main/java/genesis/world/biome/BiomeGenRainforestM.java
+++ src/main/java/genesis/world/biome/BiomeGenRainforestM.java
@@ -3,8 +3,6 @@ package genesis.world.biome;
 import genesis.common.GenesisBlocks;
 import genesis.util.random.f.FloatRange;
 import genesis.world.biome.decorate.WorldGenBoulders;
-import genesis.world.biome.decorate.WorldGenDebris;
-import genesis.world.biome.decorate.WorldGenSplash;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class BiomeGenRainforestM extends BiomeGenRainforest
@@ -12,17 +10,16 @@ public class BiomeGenRainforestM extends BiomeGenRainforest
 	public BiomeGenRainforestM(BiomeGenBase.BiomeProperties properties)
 	{
 		super(properties);
+		
+		super.addDecorations();
 	}
 	
 	@Override
 	protected void addDecorations()
 	{
-		addDecoration(WorldGenSplash.createHumusSplash(), 2.75F);
 		addDecoration(new WorldGenBoulders(1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.4F);
 		addDecoration(new WorldGenBoulders(GenesisBlocks.rhyolite.getDefaultState(), 1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.075F);
 		addDecoration(new WorldGenBoulders(GenesisBlocks.dolerite.getDefaultState(), 1, 0, 0).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.5F, 1)), 0.075F);
-		
-		addPostDecoration(new WorldGenDebris(), 28);
 	}
 	
 	@Override
