59371b5908307e5b8895521a5effe4b400021241
GirafiStudios <girafi0@gmail.com>
2016-09-11 21:52:40 +0200

    Added Mycorrhiza block model

--- src/main/java/genesis/block/BlockMycorrhiza.java
+++ src/main/java/genesis/block/BlockMycorrhiza.java
@@ -1,10 +1,40 @@
 package genesis.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlockMycorrhiza extends BlockPeat
 {
+	public static final PropertyBool SNOWY = PropertyBool.create("snowy");
+
 	public BlockMycorrhiza() {
+		super();
 		setSoundType(SoundType.PLANT);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(SNOWY, false));
+	}
+
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos)
+	{
+		Block block = world.getBlockState(pos.up()).getBlock();
+		return state.withProperty(SNOWY, block == Blocks.SNOW || block == Blocks.SNOW_LAYER);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state)
+	{
+		return 0;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState()
+	{
+		return new BlockStateContainer(this, SNOWY);
 	}
 }
