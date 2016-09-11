5ba681c1a2f9c480bac447a0147247135e0b94c1
Anaghold <benjaminbeaulieu27@gmail.com>
2016-09-12 00:01:10 +0200

    Updated peat texture.

--- src/main/java/genesis/common/GenesisBiomes.java
+++ src/main/java/genesis/common/GenesisBiomes.java
@@ -217,8 +217,8 @@ public final class GenesisBiomes
 		prop = new Biome.BiomeProperties("Marsh");
 		prop.setTemperature(1.15F);
 		prop.setRainfall(0.3F);
-		prop.setBaseHeight(-0.05F);
-		prop.setHeightVariation(-0.005F);
+		prop.setBaseHeight(0.0F);
+		prop.setHeightVariation(-0.01F);
 		prop.setWaterColor(0x725113);
 		
 		marsh = new BiomeMarsh(prop);
