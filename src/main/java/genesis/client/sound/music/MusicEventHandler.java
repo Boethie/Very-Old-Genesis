4a1ea4cb07c24e3f8a5b1330bfb1f2eeff0b985a
Fararise <fararise@gmail.com>
2016-09-13 18:26:47 +0200

    Fix vanilla music playing at same time as custom music

--- src/main/java/genesis/client/sound/music/MusicEventHandler.java
+++ src/main/java/genesis/client/sound/music/MusicEventHandler.java
@@ -7,11 +7,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.util.SoundCategory;
-
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class MusicEventHandler 
+public class MusicEventHandler
 {
 	private ISound currentMusic;
 
@@ -19,19 +18,25 @@ public class MusicEventHandler
 	public void onMusicPlay(PlaySoundEvent event)
 	{
 		Minecraft mc = Minecraft.getMinecraft();
-		
+
 		//Replace the background music with genesis music if we are in the genesis dimension
 		if (GenesisConfig.playDimensionMusic
-				&& (currentMusic == null || !event.getManager().isSoundPlaying(currentMusic))
 				&& event.getSound().getCategory() == SoundCategory.MUSIC
 				&& mc.theWorld != null
 				&& GenesisDimensions.isGenesis(mc.theWorld)
 				&& mc.gameSettings.getSoundLevel(SoundCategory.MUSIC) > 0
 				&& mc.gameSettings.getSoundLevel(SoundCategory.MASTER) > 0)
 		{
-			this.currentMusic = PositionedSoundRecord.getMusicRecord(GenesisSoundEvents.MUSIC_GENESIS);
-			this.currentMusic.createAccessor(event.getManager().sndHandler);
-			event.setResultSound(currentMusic);
+			if (currentMusic == null || !event.getManager().isSoundPlaying(currentMusic))
+			{
+				this.currentMusic = PositionedSoundRecord.getMusicRecord(GenesisSoundEvents.MUSIC_GENESIS);
+				this.currentMusic.createAccessor(event.getManager().sndHandler);
+				event.setResultSound(currentMusic);
+			}
+			else
+			{
+				event.setResultSound(null);
+			}
 		}
 	}
 }
