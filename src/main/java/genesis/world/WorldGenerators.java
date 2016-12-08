e38f8372159474b37b640549ebda8f2ac13ebb76
Anaghold <benjaminbeaulieu27@gmail.com>
2016-12-08 22:50:32 +0100

    Updated textures for komatiitic lava. Disabled the small camps for the moment.

--- src/main/java/genesis/world/WorldGenerators.java
+++ src/main/java/genesis/world/WorldGenerators.java
@@ -26,7 +26,7 @@ public class WorldGenerators
 		GameRegistry.registerWorldGenerator(new WorldGenPortal(), 0);
 		GameRegistry.registerWorldGenerator(new WorldGenMenhirActivators(), 0);
 		GameRegistry.registerWorldGenerator(new WorldGenHut(), 0);
-		GameRegistry.registerWorldGenerator(new WorldGenSmallCamp(), 0);
+		//GameRegistry.registerWorldGenerator(new WorldGenSmallCamp(), 0);
 		GameRegistry.registerWorldGenerator(new WorldGenMetaHouse(), 0);
 		
 		for (EnumMenhirActivator activator : EnumMenhirActivator.values())
