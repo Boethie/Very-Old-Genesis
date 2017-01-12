fe064e0f57dc34d3b7d46412625fb4bb3d2417ce
Anaghold <benjaminbeaulieu27@gmail.com>
2017-01-12 04:16:08 +0100

    Removed some stuff in meta forest. Updated the texture of the Metasequoia sapling.

--- src/main/java/genesis/world/biome/BiomeMetaForest.java
+++ src/main/java/genesis/world/biome/BiomeMetaForest.java
@@ -2,14 +2,12 @@ package genesis.world.biome;
 
 import java.util.Random;
 
-import genesis.combo.variant.EnumDung;
 import genesis.combo.variant.EnumPlant;
 import genesis.combo.variant.EnumTree;
 import genesis.common.GenesisBlocks;
 import genesis.util.random.f.FloatRange;
 import genesis.world.biome.decorate.WorldGenBoulders;
 import genesis.world.biome.decorate.WorldGenDebris;
-import genesis.world.biome.decorate.WorldGenDung;
 import genesis.world.biome.decorate.WorldGenGrowingPlant;
 import genesis.world.biome.decorate.WorldGenPalaeoagaracites;
 import genesis.world.biome.decorate.WorldGenPlant;
@@ -17,7 +15,6 @@ import genesis.world.biome.decorate.WorldGenRoots;
 import genesis.world.biome.decorate.WorldGenSplash;
 import genesis.world.biome.decorate.WorldGenStemonitis;
 import genesis.world.gen.feature.WorldGenDeadLog;
-import genesis.world.gen.feature.WorldGenTreeArchaeanthus;
 import genesis.world.gen.feature.WorldGenTreeBase.TreeTypes;
 import genesis.world.gen.feature.WorldGenTreeGinkgo;
 import genesis.world.gen.feature.WorldGenTreeMetasequoia;
@@ -43,7 +40,6 @@ public class BiomeMetaForest extends BiomeGenesis
 		
 		addDecoration(WorldGenSplash.createHumusSplash(), 4.1F);
 		addDecoration(new WorldGenBoulders(0.199F, 0.333F, 1).setRadius(FloatRange.create(0.75F, 1.5F), FloatRange.create(0.25F, 0.75F)), 0.21F);
-		addDecoration(new WorldGenDung(EnumDung.HERBIVORE), 0.05F);
 		
 		//TODO: WorldGenPaleogaracites should be instead called/moved inside the dead log generation.
 		addDecoration(new WorldGenPalaeoagaracites().setPatchCount(24), 14);
@@ -58,7 +54,6 @@ public class BiomeMetaForest extends BiomeGenesis
 	{
 		getDecorator().setTreeCount(4.05F);
 		
-		addTree(new WorldGenTreeArchaeanthus(9, 20, false), 4);
 		addTree(new WorldGenTreeMetasequoia(17, 22, true), 300);
 		addTree(new WorldGenTreeMetasequoia(23, 27, true).setType(TreeTypes.TYPE_2), 120);
 		addTree(new WorldGenTreeGinkgo(10, 13, false), 5);
