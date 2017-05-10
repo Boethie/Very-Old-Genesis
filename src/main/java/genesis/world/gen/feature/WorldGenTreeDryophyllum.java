6c1f226e3e899c6323b531828314bd830e2f0395
Barteks2x <Barteks2x@gmail.com>
2017-05-10 02:47:40 +0200

    Fixed too long straight up branches
    fixed some issues with detedcting too dense branches
    Currently there is still inbverted logic there but it mostly works and
    works better than when it's "fixed"

--- src/main/java/genesis/world/gen/feature/WorldGenTreeDryophyllum.java
+++ src/main/java/genesis/world/gen/feature/WorldGenTreeDryophyllum.java
@@ -1,31 +1,22 @@
 package genesis.world.gen.feature;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Set;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Sets;
 import genesis.combo.TreeBlocksAndItems;
 import genesis.combo.variant.EnumTree;
 import genesis.common.GenesisBlocks;
 import genesis.util.BlockVolumeShape;
-import genesis.util.math.Vectors;
 import genesis.util.random.i.IntRange;
 import genesis.util.random.i.WeightedIntItem;
 import genesis.util.random.i.WeightedIntProvider;
 import net.minecraft.block.BlockLog;
-import net.minecraft.block.BlockLog.EnumAxis;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import static java.lang.Math.abs;
-import static java.lang.Math.pow;
+import java.util.Random;
+
+import static java.lang.Math.*;
 
 public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 {
@@ -57,7 +48,7 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 	protected boolean doGenerate(World world, Random rand, BlockPos pos)
 	{
 		int height = heightProvider.get(rand);
-		int base = height - ((treeType == TYPE_SMALL_1 || treeType == TYPE_SMALL_2) ? 4 : 9);
+		int base = height - ((treeType == TYPE_SMALL_1 || treeType == TYPE_SMALL_2) ? 4 : 7);
 		
 		if (!BlockVolumeShape.region(-1, 1, -1, 1, base - 1, 1)
 				.and(-3, base, -3, 3, height, 3)
@@ -65,8 +56,6 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 		{
 			return false;
 		}
-		
-		// target: ~8-10 and 10-15
 		int trunkHeight = base;
 		generateTrunk(world, pos, trunkHeight);
 		if (treeType == TYPE_SMALL_1 || treeType == TYPE_SMALL_2)
@@ -87,14 +76,18 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 			double r = 5.3;
 			double yr = 9;
 			double rPart = 2.7;
-			Vec3d currentPos = new Vec3d(pos).add(
-					new Vec3d(
-							(rand.nextDouble() * 2 - 1) * r,
-							(rand.nextDouble()) * yr,
-							(rand.nextDouble() * 2 - 1) * r
-					));
+			Vec3d currentPos = new Vec3d(pos).add(new Vec3d(
+					(rand.nextDouble() * 2 - 1) * r, (rand.nextDouble()) * yr, (rand.nextDouble() * 2 - 1) * r
+			));
+			BlockPos tryPos = pos.down(rand.nextInt(5) + 2);
+			double dy = currentPos.yCoord - pos.getY();
+			if (abs(dy) > 9)
+			{
+				tryPos = tryPos.up((int) round(abs(dy) - 9));
+			}
+			
 			generateLeaves(world, rand, currentPos, rPart);
-			generateBranch(world, pos.down(rand.nextInt(5) + 2), currentPos);
+			generateBranch(world, rand, tryPos, currentPos);
 		}
 	}
 	
@@ -105,46 +98,71 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 			double r = 2.3;
 			double yr = 4;
 			double rPart = 2.7;
-			Vec3d currentPos = new Vec3d(pos).add(
-					new Vec3d(
-							(rand.nextDouble() * 2 - 1) * r,
-							(rand.nextDouble()) * yr,
-							(rand.nextDouble() * 2 - 1) * r
-					));
+			Vec3d currentPos = new Vec3d(pos).add(new Vec3d(
+					(rand.nextDouble() * 2 - 1) * r, (rand.nextDouble()) * yr, (rand.nextDouble() * 2 - 1) * r
+			));
 			generateLeaves(world, rand, currentPos, rPart);
-			generateBranch(world, pos.down(rand.nextInt(5) + 1), currentPos);
+			generateBranch(world, rand, pos.down(rand.nextInt(4) + 1), currentPos);
 		}
 	}
 	
-	private void generateBranch(World world, BlockPos trunkPos, Vec3d endPos)
+	private void generateBranch(World world, Random rand, BlockPos trunkPos, Vec3d endPos)
 	{
 		Vec3d curr = new Vec3d(offsetHorizontallyTowards(trunkPos, endPos));
 		Vec3d diff = endPos.subtract(curr);
 		Vec3d dir = diff.normalize();
 		
+		BlockPos prev = null;
 		while (curr.subtract(endPos).dotProduct(new Vec3d(1, 1, 1)) < 0)
 		{
 			BlockPos p = new BlockPos(curr);
-			
 			Vec3d next = next(curr, dir);
+			
 			if (!hasSpace(world, p, trunkPos) && hasNeighbor(world, new BlockPos(next)))
 			{
 				curr = next;
 				continue;
 			}
+			
 			IBlockState state = wood.withProperty(BlockLog.LOG_AXIS, getLogAxis(world, p));
 			setBlockInWorld(world, p, state, true);
-			curr = next;
+			
+			// check to avoid long straight up branches
+			BlockPos nextBlock = new BlockPos(next);
+			if (prev != null && prev.getX() == p.getX() && prev.getZ() == p.getZ() && p.getX() == nextBlock.getX() && p.getZ() == nextBlock.getZ())
+			{
+				prev = p;
+				curr = next.addVector(rand.nextBoolean() ? -1 : 1, 0, rand.nextBoolean() ? -1 : 1);
+				dir = endPos.subtract(curr).normalize();
+			}
+			else
+			{
+				prev = p;
+				curr = next;
+			}
 		}
 	}
 	
 	private BlockPos offsetHorizontallyTowards(BlockPos pos, Vec3d endPos)
 	{
+		BlockPos endBlock = new BlockPos(endPos);
+		if (endBlock.equals(pos))
+		{
+			return pos;
+		}
 		double dx = endPos.xCoord - pos.getX();
 		double dz = endPos.zCoord - pos.getZ();
-		dx = Math.signum(dx);
-		dz = Math.signum(dz);
-		return pos.add(Math.round((float) dx), 0, Math.round((float) dz));
+		dx = Math.signum(Math.round(dx));
+		dz = Math.signum(Math.round(dz));
+		if (endBlock.getX() == pos.getX())
+		{
+			return pos.add(0, 0, (int) dz);
+		}
+		if (endBlock.getZ() == pos.getZ())
+		{
+			return pos.add((int) dx, 0, 0);
+		}
+		return pos.add((int) dx, 0, (int) dz);
 	}
 	
 	private BlockLog.EnumAxis getLogAxis(World world, BlockPos pos)
@@ -157,24 +175,13 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 			{
 				for (int dz = -1; dz <= 1; dz++)
 				{
-					if (dx == 0 && dy == 0 && dz == 0)
+					if (dx != 0 || dy != 0 || dz != 0)
 					{
-						continue;
-					}
-					
-					if (world.getBlockState(pos.add(dx, dy, dz)).getBlock() == wood.getBlock())
-					{
-						if (dx != 0)
+						if (world.getBlockState(pos.add(dx, dy, dz)).getBlock() == wood.getBlock())
 						{
-							weightX++;
-						}
-						if (dy != 0)
-						{
-							weightY++;
-						}
-						if (dz != 0)
-						{
-							weightZ++;
+							weightX += dx == 0 ? 0 : 1;
+							weightY += dy == 0 ? 0 : 1;
+							weightZ += dz == 0 ? 0 : 1;
 						}
 					}
 				}
@@ -202,6 +209,7 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 		return BlockLog.EnumAxis.NONE;
 	}
 	
+	
 	private boolean hasNeighbor(World world, BlockPos pos)
 	{
 		for (int dx = -1; dx <= 1; dx++)
@@ -226,12 +234,9 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 	
 	private boolean hasSpace(World world, BlockPos pos, BlockPos trunkPos)
 	{
-		int total = hasSpaceCount(world, pos.up(), trunkPos) +
-				hasSpaceCount(world, pos.down(), trunkPos) +
-				hasSpaceCount(world, pos.east(), trunkPos) +
-				hasSpaceCount(world, pos.west(), trunkPos) +
-				hasSpaceCount(world, pos.north(), trunkPos) +
-				hasSpaceCount(world, pos.south(), trunkPos);
+		int total = max(hasSpaceCount(world, pos.up(), trunkPos), hasSpaceCount(world, pos.down(), trunkPos)) +
+				max(hasSpaceCount(world, pos.east(), trunkPos), hasSpaceCount(world, pos.west(), trunkPos)) +
+				max(hasSpaceCount(world, pos.north(), trunkPos), hasSpaceCount(world, pos.south(), trunkPos));
 		return total < 2;
 	}
 	
@@ -241,7 +246,7 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 		{
 			return 0;
 		}
-		return (world.getBlockState(pos.down()).getBlock() != wood.getBlock() ? 1 : 0);
+		return (world.getBlockState(pos).getBlock() != wood.getBlock() ? 1 : 0);
 	}
 	
 	private Vec3d next(Vec3d pos, Vec3d direction)
@@ -272,7 +277,6 @@ public class WorldGenTreeDryophyllum extends WorldGenTreeBase
 			}
 		}
 	}
-	
 	
 	private void generateTrunk(World world, BlockPos pos, int height)
 	{
