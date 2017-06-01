60341df22e88b7e7736787ed306ee522e87cbde8
The_Fireplace <the.f1repl4ce@gmail.com>
2017-06-01 13:49:01 -0500

    Consistency with the rest of the project

--- src/main/java/genesis/block/BlockSmoker.java
+++ src/main/java/genesis/block/BlockSmoker.java
@@ -29,103 +29,100 @@ import java.util.Random;
 public class BlockSmoker extends BlockGenesis
 {
 
-    public static final PropertyInteger LEVEL = PropertyInteger.create("level", 0, 15);//So the game doesn't crash
+	public static final PropertyInteger LEVEL = PropertyInteger.create("level", 0, 15);//So the game doesn't crash
 
-    public BlockSmoker()
-    {
-        super(Material.WATER, SoundType.STONE);
-        setCreativeTab(GenesisCreativeTabs.DECORATIONS);
-        setUnlocalizedName(Constants.Unlocalized.PREFIX + "smoker");
-        setHardness(1.5F);
-        setResistance(10.0F);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(LEVEL, Integer.valueOf(0)));
-        setTickRandomly(true);
-        setHarvestLevel("pickaxe", 0);
-    }
+	public BlockSmoker(Material material, SoundType sound)
+	{
+		super(material, sound);
+		setCreativeTab(GenesisCreativeTabs.DECORATIONS);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(LEVEL, Integer.valueOf(0)));
+		setTickRandomly(true);
+		setHarvestLevel("pickaxe", 0);
+	}
 
-    @Override
-    public int tickRate(World worldIn)
-    {
-        return 300;
-    }
+	@Override
+	public int tickRate(World worldIn)
+	{
+		return 300;
+	}
 
-    @Override
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
-    {
-        return new AxisAlignedBB(0.25F, 0F, 0.25F, 0.75F, 1.0F, 0.75F);
-    }
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+	{
+		return new AxisAlignedBB(0.25F, 0F, 0.25F, 0.75F, 1.0F, 0.75F);
+	}
 
-    @Override
-    public boolean isOpaqueCube(IBlockState state)
-    {
-        return false;
-    }
+	@Override
+	public boolean isOpaqueCube(IBlockState state)
+	{
+		return false;
+	}
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getBlockLayer()
-    {
-        return BlockRenderLayer.CUTOUT;
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer()
+	{
+		return BlockRenderLayer.CUTOUT;
+	}
 
-    @Override
-    public IBlockState getStateFromMeta(int meta)
-    {
-        return this.getDefaultState().withProperty(LEVEL, Integer.valueOf(meta));
-    }
+	@Override
+	public IBlockState getStateFromMeta(int meta)
+	{
+		return this.getDefaultState().withProperty(LEVEL, Integer.valueOf(meta));
+	}
 
-    @Override
-    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
-    {
-        return FULL_BLOCK_AABB;//Prevent the player from entering the block, because Material Water causes them to be able to swim through it. More of a bandaid than a real fix.
-    }
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, World worldIn, BlockPos pos)
+	{
+		return FULL_BLOCK_AABB;//Prevent the player from entering the block, because Material Water causes them to be able to swim through it. More of a bandaid than a real fix.
+	}
 
-    @Override
-    public int getMetaFromState(IBlockState state)
-    {
-        return ((Integer)state.getValue(LEVEL)).intValue();
-    }
+	@Override
+	public int getMetaFromState(IBlockState state)
+	{
+		return ((Integer)state.getValue(LEVEL)).intValue();
+	}
 
-    @Override
-    protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[] {LEVEL});
-    }
+	@Override
+	protected BlockStateContainer createBlockState()
+	{
+		return new BlockStateContainer(this, new IProperty[] {LEVEL});
+	}
 
-    @Override
-    public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos)
-    {
-        return false;
-    }
+	@Override
+	public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos)
+	{
+		return false;
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
-    {
-        if (worldIn.getBlockState(pos.up()).getMaterial().isLiquid() && worldIn.getBlockState(pos.up()).getBlock() != GenesisBlocks.SMOKER)
-        {
-            Random random = worldIn.rand;
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void randomDisplayTick(IBlockState stateIn, World worldIn, BlockPos pos, Random rand)
+	{
+		if (worldIn.getBlockState(pos.up()).getMaterial().isLiquid() && worldIn.getBlockState(pos.up()).getBlock() != GenesisBlocks.SMOKER)
+		{
+			Random random = worldIn.rand;
 
-            for (int i = 0; i < 8; ++i)
-            {
-                double x = pos.getX() + 0.5;
-                double y = pos.getY() + i * 0.1;
-                double z = pos.getZ() + 0.5;
+			for (int i = 0; i < 8; ++i)
+			{
+				double x = pos.getX() + 0.5;
+				double y = pos.getY() + i * 0.1;
+				double z = pos.getZ() + 0.5;
 
-                worldIn.spawnParticle(random.nextInt(10) == 0 ? EnumParticleTypes.WATER_BUBBLE : EnumParticleTypes.SMOKE_LARGE, x, y, z, 0.0D, 0.1D, 0.0D);
-            }
-        }
-    }
+				worldIn.spawnParticle(random.nextInt(10) == 0 ? EnumParticleTypes.WATER_BUBBLE : EnumParticleTypes.SMOKE_LARGE, x, y, z, 0.0D, 0.01D, 0.0D);
+			}
+		}
+	}
 
-    @Override
-    protected boolean canSilkHarvest()
-    {
-        return true;
-    }
+	@Override
+	protected boolean canSilkHarvest()
+	{
+		return true;
+	}
 
-    @Override
-    public Item getItemDropped(IBlockState state, Random rand, int fortune)
-    {
-        return null;
-    }
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune)
+	{
+		return null;
+	}
 }
