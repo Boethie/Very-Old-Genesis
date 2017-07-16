4fd33b7dbd0a0ae6a7f5937486dd6f40758da305
Anaghold <benjaminbeaulieu27@gmail.com>
2017-07-16 19:28:44 +0200

    Re-deleted roots for Archaeopteris tree.

--- src/main/java/genesis/world/gen/feature/WorldGenTreeArchaeopteris.java
+++ src/main/java/genesis/world/gen/feature/WorldGenTreeArchaeopteris.java
@@ -1,12 +1,11 @@
 package genesis.world.gen.feature;
 
-import java.util.Random;
-
 import genesis.combo.variant.EnumTree;
 import genesis.util.BlockVolumeShape;
 import genesis.util.random.i.IntRange;
-import net.minecraft.block.BlockLog;
-import net.minecraft.util.EnumFacing;
+
+import java.util.Random;
+
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -28,68 +27,9 @@ public class WorldGenTreeArchaeopteris extends WorldGenTreeBase
 					 .hasSpace(pos, isEmptySpace(world)))
 			return false;
 		
-		BlockPos checkPos = pos;
-		
-		if (treeType == TreeTypes.TYPE_2
-				&& (getTreePos(world, checkPos = checkPos.east(), 1) == null
-					|| getTreePos(world, checkPos = checkPos.south(), 1) == null)
-					|| getTreePos(world, checkPos = checkPos.west(), 1) == null)
-			return false;
-		
-		for (BlockPos cornerPos : BlockPos.getAllInBoxMutable(pos, pos.add(1, 0, 1)))
-		{
-			if (cornerPos.equals(pos))
-				continue;
-			
-			BlockPos groundPos = getTreePos(world, cornerPos);
-			
-			if (groundPos == null)
-				return false;
-			
-			pos = new BlockPos(pos.getX(), Math.min(pos.getY(), groundPos.getY()), pos.getZ());
-		}
-		
-		int rootMaxHeight = 3;
-		int[] roots = {rand.nextInt(rootMaxHeight), rand.nextInt(rootMaxHeight), rand.nextInt(rootMaxHeight), rand.nextInt(rootMaxHeight)};
-		
-		if ((roots[0] + roots[1] + roots[2] + roots[3]) / 4 == roots[0] * 4)
-		{
-			int rand_root = rand.nextInt(rootMaxHeight);
-			int rand_height = rand.nextInt(30) % (rootMaxHeight - 1);
-			roots[rand_root] = rand_height;
-			
-			if (rand_height - 1 > 0)
-			{
-				rand_root = (rand_root + 1) % (rootMaxHeight - 1);
-				roots[rand_root] = rand_height - 1;
-			}
-		}
-		
 		for (int i = 0; i < height; i++)
 		{
-			switch (treeType)
-			{
-			case TYPE_2:
-				setBlockInWorld(world, pos.add(1, i, 0), wood);
-				setBlockInWorld(world, pos.add(0, i, 1), wood);
-				setBlockInWorld(world, pos.add(1, i, 1), wood);
-				setBlockInWorld(world, pos.add(0, i, 0), wood);
-				break;
-			default:
-				if (rand.nextInt(2) == 0)
-				{
-					for (EnumFacing f : EnumFacing.HORIZONTALS)
-					{
-						if (i < roots[f.getHorizontalIndex()])
-						{
-							setBlockInWorld(world, pos.offset(f).up(i), wood.withProperty(BlockLog.LOG_AXIS, BlockLog.EnumAxis.NONE));
-						}
-					}
-				}
-				
-				setBlockInWorld(world, pos.up(i), wood);
-				break;
-			}
+			setBlockInWorld(world, pos.up(i), wood);
 		}
 		
 		BlockPos branchPos = pos.up(height - 1);
